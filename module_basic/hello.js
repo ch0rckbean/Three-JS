9b375fbe89d6dca8e49d3294e7737f69b469d920
Bean <86273626+ch0rckbean@users.noreply.github.com>
2024-02-21 20:57:44 +0900

    study: js Module

---
+++ module_basic/hello.js
@@ -0,0 +1,10 @@
+// 모듈화
+// export default function hello1() {
+// import 시 { } 안 써도 됨
+export function hello1() {
+  console.log('hello1');
+}
+
+export function hello2() {
+  console.log('hello2');
+}
---
+++ module_basic/main.js
@@ -0,0 +1,9 @@
+// import { hello1, hello2 } from './hello.js';
+import * as hello from './hello.js';
+// -- 모듈화 --
+
+// hello1();
+// hello2();
+
+hello.hello1();
+hello.hello2();
